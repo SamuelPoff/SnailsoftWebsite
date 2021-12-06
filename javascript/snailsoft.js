9a7e04b17acc7b3e8f9f1abf57b1dcd185dec975
Orengee <46472495+Orengee@users.noreply.github.com>
2021-12-06 01:59:16 -0500

    Hamburger Button Added, Mobile Navigation changed to vertical layout

--- javascript/snailsoft.js
+++ javascript/snailsoft.js
@@ -1,11 +1,39 @@
-function main(){
 
-    let array = [1];
-    let additions = [2, 3, 4, 5];
-    array.push.apply(...additions);
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const navMenuItems = document.getElementsByClassName("NavItem");
 
-    console.log(array);
-
+function ToggleNavMenu(){
+    for(var i = 0;i < navMenuItems.length; i++){
+        navMenuItems[i].classList.toggle("hide");
+    }
 }
 
-main();
+hamburgerBtn.addEventListener("click", function(){
+
+    ToggleNavMenu();
+    
+});
+
+function ElementsHidden(){
+    return (navMenuItems[0].classList.contains("hide"));
+}
+
+
+window.addEventListener("resize", function(event){
+
+    if( ElementsHidden() ){
+
+        let width = document.documentElement.clientWidth;
+        let height = document.documentElement.clientHeight;
+    
+        if(width >= 600){
+            
+            //Hidden menu elements need to be revealed again
+            ToggleNavMenu();
+    
+        }
+
+    }
+    
+
+});
